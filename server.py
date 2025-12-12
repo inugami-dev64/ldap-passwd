ab7b134668b0f7b566a542fb4277cfd3d58b62c1
inugami-dev64 <karmen@nyaa.ee>
2025-12-13 00:23:02 +0200

    Add logging

--- server.py
+++ server.py
@@ -16,12 +16,13 @@ def post_pwd_change():
     username = request.form['username']
     currentPassword = request.form['currentPassword']
     newPassword = request.form['newPassword']
+    app.logger.info(f"Changing password for user '{username}'")
 
     try:
         try:
             server = Server('127.0.0.1', port=3389, get_info=ALL)
         except Exception as e:
-            print(e)
+            app.logger.error(f"Failed to establish LDAP connection: {e}")
             return render_template('index.html', errmsg="Internal server error"), 500
 
         # TODO: Replace it with a template read from dotenv and do escaping
@@ -32,5 +33,5 @@ def post_pwd_change():
         conn.extend.standard.modify_password(dn, currentPassword, newPassword)
         return render_template('index.html', successmsg="Password changed successfully, you can login to services with new password!")
     except Exception as e:
-        print(e)
+        app.logger.warning(f"Failed to change password for user '{username}': {e}")
         return render_template('index.html', errmsg="Invalid username or password"), 400
